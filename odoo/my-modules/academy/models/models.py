eab42b9f96be713e5f8abf4b2b317de214a33d7c
huudanh2808 <huudanh2808@gmail.com>
2018-01-22 22:32:03 +0700

    update form for teacher and courses, courses form have discussion area under it

--- odoo/my-modules/academy/models/models.py
+++ odoo/my-modules/academy/models/models.py
@@ -17,3 +17,10 @@ class Teachers(models.Model):
     _name = 'academy.teachers'
     name = fields.Char()
     biography = fields.Html()
+    course_ids = fields.One2many('academy.courses', 'teacher_id', string="Courses")
+
+class Courses(models.Model):
+    _name = 'academy.courses'
+    _inherit = 'mail.thread'
+    name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
